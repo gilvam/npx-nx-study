11777a7d83ecd20fe827ca4f900437ce68d8c898
Gilvam Mourão <gilvamm@gmail.com>
2021-07-07 17:07:10 -0300

    initial commit

---
+++ apps/npx-nx/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'npx-nx-study-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'npx-nx';
+}
